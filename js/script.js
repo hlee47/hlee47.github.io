2c0ec0cc849b3251cbd8854b0908a61652c59af5
FULLNAM-3S79DFN\Administrator <hankeunlee910924@gmail.com>
2017-09-09 11:13:19 +0900

    no message

---
+++ js/script.js
@@ -0,0 +1,22 @@
+/*
+    1. https://github.com 에 회원가입을 한다.
+    2. https://www.sourcetreeapp.com/ 툴 다운로드
+    3. 소스트리를 설치 하면서 가입을 한다.
+    4. github에 레파지토리(repository)를 만든다.
+    5. markdown(언어) 파일을 정리한다.
+*/
+
+/*
+    sourcetree에서 clone이라는 걸 사용할 거다.
+    clone을 눌러서 github 웹사이에 들어가보면 초록색clone or download?라는 버튼이 있는데 그거 누르면 url이 저장된다? 그래서 그 url을 sourcetree에 copy and paste해서 폴더 지정하고 클론을 누르면 ...
+    
+*/
+
+/*
+    http://opencart.templatemela.com/OPC09/OPC090210/
+    를 사용한단다...
+*/
+
+/*
+    github에 들어가서 내 아이디 클릭, repository tab을 클릭해서 새로 하나 만들기 들어가서 repository name에다가 hlee47.github.io (앞에 hlee47는 아무거나 적어도 되는데 뒤에 .github.io를 꼭 똑같이 적어야 한다.) 그 다음 만들어 준다.
+*/
